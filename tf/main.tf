012cada94d30ce68c0fcb0f221382be2863746a4
Ken Sykora <ken.sykora@gmail.com>
2021-11-07 13:02:52 -0600

    Add Backups

--- tf/main.tf
+++ tf/main.tf
@@ -36,3 +36,27 @@ resource "azurerm_storage_container" "backup" {
   storage_account_name = azurerm_storage_account.main.name
   name                 = "backup"
 }
+
+resource "azurerm_storage_management_policy" "expire_backups" {
+  storage_account_id = azurerm_storage_account.main.id
+
+  rule {
+    name    = "ExpireAfter14Days"
+    enabled = true
+    filters {
+      prefix_match = ["*"]
+      blob_types   = ["blockBlob"]
+    }
+    actions {
+      base_blob {
+        delete_after_days_since_modification_greater_than = 14
+      }
+      snapshot {
+        delete_after_days_since_creation_greater_than = 14
+      }
+      version {
+        delete_after_days_since_creation = 14
+      }
+    }
+  }
+}
